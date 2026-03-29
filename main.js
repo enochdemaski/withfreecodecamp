e3687d604be3ba355fffb1205a75c3419bdd2b81
Ibe Enoch Okechukwu <ibeenochokey@gmail.com>
2026-03-29 13:24:33 +0100

    update

--- main.js
+++ main.js
@@ -1,92 +1,92 @@
-// const video = document.getElementById("video");
-// const menuBtn = document.getElementById("toggle-btn");
-// const mobileMenu = document.getElementById("menu");
-// const darkMode = document.getElementById("dark-mode-btn");
-// const nav = document.getElementById("nav");
-// const logo = document.querySelector("#nav a #logo");
-// const icon = darkMode.querySelector("i");
-// const icons = menuBtn.querySelector("i");
-// const greetCustomer = document.querySelector("#greet-customers");
-// const contact = document.getElementById("reach");
-// const know = document.getElementById("know");
-// const track = document.querySelector(".carousel-container");
-// const nextBtn = document.querySelector(".next");
-// const prevBtn = document.querySelector(".prev");
+const video = document.getElementById("video");
+const menuBtn = document.getElementById("toggle-btn");
+const mobileMenu = document.getElementById("menu");
+const darkMode = document.getElementById("dark-mode-btn");
+const nav = document.getElementById("nav");
+const logo = document.querySelector("#nav a #logo");
+const icon = darkMode.querySelector("i");
+const icons = menuBtn.querySelector("i");
+const greetCustomer = document.querySelector("#greet-customers");
+const contact = document.getElementById("reach");
+const know = document.getElementById("know");
+const track = document.querySelector(".carousel-container");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
 
 //MENU BTN
-// menuBtn.addEventListener("click", () => {
-//   mobileMenu.classList.toggle("show");
-//   if (icons.classList.contains("fa-bars")) {
-//     icons.classList.replace("fa-bars", "fa-xmark");
-//   } else {
-//     icons.classList.replace("fa-xmark", "fa-bars");
-//   }
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+  if (icons.classList.contains("fa-bars")) {
+    icons.classList.replace("fa-bars", "fa-xmark");
+  } else {
+    icons.classList.replace("fa-xmark", "fa-bars");
+  }
 //   icons.classList.toggle("fa-bars");
 //   icons.classList.toggle("fa-xmark");
-// });
+});
 
-// document.addEventListener("click", (e) => {
-//   if (mobileMenu.contains(e.target) || !menuBtn.contains(e.target)) {
-//     mobileMenu.classList.remove("show");
-//     icons.classList.replace("fa-xmark", "fa-bars");
-//   }
-// });
+document.addEventListener("click", (e) => {
+  if (mobileMenu.contains(e.target) || !menuBtn.contains(e.target)) {
+    mobileMenu.classList.remove("show");
+    icons.classList.replace("fa-xmark", "fa-bars");
+  }
+});
 
 // FOR DARK AND LIGHT MODE
-// let isDark = false;
-// darkMode.addEventListener("click", () => {
-//   document.body.classList.toggle("dark-mode");
-//   isDark = !isDark;
+let isDark = false;
+darkMode.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  isDark = !isDark;
 
-//   if (isDark) {
-//     icon.classList.replace("fa-moon", "fa-sun");
-//     darkMode.style.backgroundColor = "#fff";
-//     darkMode.style.color = "#000";
-//     nav.style.backgroundColor = "#fff";
-//     logo.style.color = "#000";
-//     contact.style.color = "#fff";
-//     know.style.color = "#fff";
-//     know.style.backgroundColor = "#000";
-//   } else {
-//     icon.classList.replace("fa-sun", "fa-moon");
-//     darkMode.style.backgroundColor = "#000";
-//     darkMode.style.color = "#fff";
-//     nav.style.backgroundColor = "#000";
-//     logo.style.color = "#fff";
-//     contact.style.color = "#000";
-//     know.style.color = "#000";
-//     know.style.backgroundColor = "#fff";
-//   }
-// });
+  if (isDark) {
+    icon.classList.replace("fa-moon", "fa-sun");
+    darkMode.style.backgroundColor = "#fff";
+    darkMode.style.color = "#000";
+    nav.style.backgroundColor = "#fff";
+    logo.style.color = "#000";
+    contact.style.color = "#fff";
+    know.style.color = "#fff";
+    know.style.backgroundColor = "#000";
+  } else {
+    icon.classList.replace("fa-sun", "fa-moon");
+    darkMode.style.backgroundColor = "#000";
+    darkMode.style.color = "#fff";
+    nav.style.backgroundColor = "#000";
+    logo.style.color = "#fff";
+    contact.style.color = "#000";
+    know.style.color = "#000";
+    know.style.backgroundColor = "#fff";
+  }
+});
 
 // TIME GREETING
-// setTimeout(() => {
-//   const currentHour = new Date().getHours();
-//   let welcomeText = "";
+setTimeout(() => {
+  const currentHour = new Date().getHours();
+  let welcomeText = "";
 
-//   if (currentHour < 12) {
-//     welcomeText = "Good Morning, welcome to my portfolio.";
-//   } else if (currentHour < 18) {
-//     welcomeText = "Good Afternoon, welcome to my portfolio.";
-//   } else {
-//     welcomeText = "Good Evening, welcome to my portfolio.";
-//   }
-//   greetCustomer.textContent = welcomeText;
+  if (currentHour < 12) {
+    welcomeText = "Good Morning, welcome to my portfolio.";
+  } else if (currentHour < 18) {
+    welcomeText = "Good Afternoon, welcome to my portfolio.";
+  } else {
+    welcomeText = "Good Evening, welcome to my portfolio.";
+  }
+  greetCustomer.textContent = welcomeText;
 
-//   setTimeout(() => {
-//     greetCustomer.remove();
-//   }, 2000);
-// }, 1000);
+  setTimeout(() => {
+    greetCustomer.remove();
+  }, 2000);
+}, 1000);
 
 /*CAROUSEL____________________________________------_______--- */
 
-// nextBtn.addEventListener("click", () => {
-//   track.scrollBy({ left: track.offsetWidth, behavior: "smooth" });
-// });
+nextBtn.addEventListener("click", () => {
+  track.scrollBy({ left: track.offsetWidth, behavior: "smooth" });
+});
 
-// prevBtn.addEventListener("click", () => {
-//   track.scrollBy({ left: -track.offsetWidth, behavior: "smooth" });
-// });
+prevBtn.addEventListener("click", () => {
+  track.scrollBy({ left: -track.offsetWidth, behavior: "smooth" });
+});
 
 
 //FOR FORM VALIDATION
